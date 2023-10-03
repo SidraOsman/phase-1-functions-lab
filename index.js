55807ec5830b06f8bbff654d2f5eb3ecf587d388
SidraOsman <osmansidra4@gmail.com>
2023-10-03 22:47:35 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(numberOfBlocks){
+ if(numberOfBlocks > 42){
+    return numberOfBlocks - 42;
+ }else{
+    return 42 - numberOfBlocks;
+ }
+}
+
+function distanceFromHqInFeet(numberOfBlocks){
+    return distanceFromHqInBlocks(numberOfBlocks) * 264;
+}
+
+function distanceTravelledInFeet(start,destination){
+    if(start<destination){
+        return (destination - start) * 264;
+    }else{
+        return (start - destination)*264;
+    }
+}
+
+function calculatesFarePrice(start,destination){
+    const distance = distanceTravelledInFeet(start,destination);
+
+    if(distance<=400){
+        return 0;
+    }else if(400 < distance && distance<2000){
+        return 0.02 * (distance-400);
+    }else if (2000 < distance && distance < 2500){
+        return 25;
+    }else {
+        return 'cannot travel that far'
+    }
+}
